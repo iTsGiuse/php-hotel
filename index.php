96728cc86b5e1b70cf5f3707ae77561dafeb3693
iTsGiuse <giuse.amato@icloud.com>
2024-05-02 18:48:29 +0200

    feat: add bonus

--- index.php
+++ index.php
@@ -38,10 +38,31 @@ $hotels = [
     ],
 ];
 
+$filtroParcheggio = isset($_GET["sceltaParcheggio"]) && $_GET["sceltaParcheggio"] !== "prendefinito" ? $_GET["sceltaParcheggio"] : false;
+$filtroVoto = isset($_GET["sceltaVoto"]) && $_GET["sceltaVoto"] !== "prendefinito" ? $_GET["sceltaVoto"] : false;
+
+$hotelFiltrato = $hotels; 
+
+if ($filtroParcheggio || $filtroVoto) {
+    $hotelFiltrato = [];
+    foreach ($hotels as $hotel) {
+        if ($filtroParcheggio && $filtroVoto) {
+            if ($hotel["parking"] && $hotel["vote"] >= $filtroVoto) {
+                $hotelFiltrato[] = $hotel;
+            }
+        } elseif ($filtroParcheggio) {
+            if ($hotel["parking"]) {
+                $hotelFiltrato[] = $hotel;
+            }
+        } elseif ($filtroVoto) {
+            if ($hotel["vote"] >= $filtroVoto) {
+                $hotelFiltrato[] = $hotel;
+            }
+        }
+    }
+}
+
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -74,42 +95,104 @@ $hotels = [
                         <td><?= $hotels[0]['description'] ?></td>
                         <td><?= $hotels[0]['parking'] ?></td>
                         <td><?= $hotels[0]['vote'] ?></td>
-                        <td><?= $hotels[0]['distance_to_center'] ?></td>
+                        <td><?= $hotels[0]['distance_to_center'] ?> km</td>
                     </tr>
                     <tr>
                         <th><?= $hotels[1]['name'] ?></th>
                         <td><?= $hotels[1]['description'] ?></td>
                         <td><?= $hotels[1]['parking'] ?></td>
                         <td><?= $hotels[1]['vote'] ?></td>
-                        <td><?= $hotels[1]['distance_to_center'] ?></td>
+                        <td><?= $hotels[1]['distance_to_center'] ?> km</td>
                     </tr>
                     <tr>
                         <th><?= $hotels[2]['name'] ?></th>
                         <td><?= $hotels[2]['description'] ?></td>
                         <td><?= $hotels[2]['parking'] ?></td>
                         <td><?= $hotels[2]['vote'] ?></td>
-                        <td><?= $hotels[2]['distance_to_center'] ?></td>
+                        <td><?= $hotels[2]['distance_to_center'] ?> km</td>
                     </tr>
                     <tr>
                         <th><?= $hotels[3]['name'] ?></th>
                         <td><?= $hotels[3]['description'] ?></td>
                         <td><?= $hotels[3]['parking'] ?></td>
                         <td><?= $hotels[3]['vote'] ?></td>
-                        <td><?= $hotels[3]['distance_to_center'] ?></td>
+                        <td><?= $hotels[3]['distance_to_center'] ?> km</td>
                     </tr>
                     <tr>
                         <th><?= $hotels[4]['name'] ?></th>
                         <td><?= $hotels[4]['description'] ?></td>
                         <td><?= $hotels[4]['parking'] ?></td>
                         <td><?= $hotels[4]['vote'] ?></td>
-                        <td><?= $hotels[4]['distance_to_center'] ?></td>
+                        <td><?= $hotels[4]['distance_to_center'] ?> km</td>
                     </tr>
                 </tbody>
             </table>
         </div>
+
+
+        <div class="container mt-5">
+            <div class="row">
+                <h2 class="text-center text-danger">BONUS</h2>
+            </div>
+            <div class="row mt-3">
+                <h4 class="fw-bolder">Filtri</h4>
+            </div>
+            <div class="row mt-3">
+                <form action="index.php" method="GET"> 
+                    <div class="row">
+                        <div class="col-5 d-flex align-items-center">
+                            <label class="fw-bold" for="sceltaParcheggio">Parcheggio</label>
+                            <select class="form-select ms-3" name="sceltaParcheggio" id="sceltaParcheggio">
+                                <option value="prendefinito" selected>Scegli...</option>
+                                <option value="si">Si</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                        <div class="col-5 d-flex align-items-center">
+                            <label class="fw-bold" for="sceltaVoto">Voto</label>
+                            <select class="form-select ms-3" name="sceltaVoto" id="sceltaVoto">
+                                <option value="prendefinito" selected>Scegli...</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                        </div>
+                        <div class="col-2">
+                            <button class="p-1 ps-3 pe-3" type="submit">FILTRA</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="row mt-3">
+                <div class="col">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Parking</th>
+                                <th>Vote</th>
+                                <th>Distance to center</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($hotelFiltrato as $hotel): ?>
+                                <tr>
+                                    <td><?= $hotel['name'] ?></td>
+                                    <td><?= $hotel['description'] ?></td>
+                                    <td><?= $hotel['parking'] ? 'Yes' : 'No' ?></td>
+                                    <td><?= $hotel['vote'] ?></td>
+                                    <td><?= $hotel['distance_to_center'] ?> km</td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </main>
-
-
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
@@ -117,7 +200,7 @@ $hotels = [
 
 
 <style>
-
+    
     tbody tr th:nth-child(1){
         font-weight: normal !important;
     }
